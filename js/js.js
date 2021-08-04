d63f5bd6871fbe7cd2256786bff8e2e4c6a5d7e4
Shreya Sharma <sshreyasharma53@gmail.com>
2021-08-04 23:37:23 +0530

    initial

--- js/js.js
+++ js/js.js
@@ -1,26 +1,25 @@
 let section;
+let arr = [1, 2, 3, 4];
 function start(name, sec) {
   console.log(`Hey ${name}. You are welcome to section ${sec}`);
   section = sec;
 }
-
 
 function exams() {
   switch (section) {
     case "A":
       console.log("You are in section A. Your class teacher is Shweta");
       break;
-      case "B":
-        console.log("You are in section B. Your class teacher is Riya");
-        break;
-        case "C":
+    case "B":
+      console.log("You are in section B. Your class teacher is Riya");
+      break;
+    case "C":
       console.log("You are in section C. Your class teacher is Bianca");
       break;
     default:
-      console.log("You are new here.. Pls get your section.")
+      console.log("You are new here.. Pls get your section.");
       break;
   }
-  
 }
 start("Shreya Sharma", "A");
 exams();
@@ -28,3 +27,6 @@ start("Bharti", "B");
 exams();
 start("Yash", "C");
 exams();
+
+arr.splice(1, 2);
+console.log(arr);
